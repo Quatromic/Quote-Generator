011cd6e0fcbf2f31ad07e8d3194a4f6eb905bc6f
Lawrence Muchiri <davidmuchiri@Network.local>
2024-04-11 11:19:20 +0300

    Added some final touches to the javascript file

--- Client/script.js
+++ Client/script.js
@@ -1,8 +1,10 @@
 let quoteContainer = document.getElementById("quote")
 let quotebtn = document.querySelector('button')
-let randomNumber = Math.floor(Math.random() * 16)
-console.log(randomNumber)
-let Quotes
+let Quotes,randomNumber
+
+setInterval(() => {
+    randomNumber = Math.floor(Math.random() * 16)
+}, 1);
 
 const quoteFetcher = async () => {
     await fetch("https://type.fit/api/quotes")
@@ -16,23 +18,29 @@ const quoteFetcher = async () => {
 
 const caller = async () => {
     const quotes = await quoteFetcher()
+    console.log("Got called")
 }
+
+let quoteTag = document.createElement('p'),
+    authorTag = document.createElement('p')
+
+quoteContainer.appendChild(quoteTag)
+quoteContainer.appendChild(authorTag)
 
 quotebtn.addEventListener("click",async () => {
     await caller()
-    console.log(Quotes)
-    let quoteTag = document.createElement('p'),
-        authorTag = document.createElement('p')
-
     quoteTag.style.fontFamily = "Protest Riot"
     authorTag.style.fontFamily = "Dancing Script"
     authorTag.style.textDecoration = "Underline"
     authorTag.style.textAlign = 'center'
     quoteTag.style.textAlign = 'center'
     
-    quoteTag.innerText = Quotes[randomNumber].text
-    authorTag.innerText = Quotes[randomNumber].author
-
-    quoteContainer.appendChild(quoteTag)
-    quoteContainer.appendChild(authorTag)
+    if(Array.isArray(Quotes)){
+        quoteTag.innerText = Quotes[randomNumber].text
+        authorTag.innerText = Quotes[randomNumber].author
+    }
+    else{
+        quoteTag.innerText = "Quotes isn't received from the request.";
+        authorTag.innerText = "Reload your page"
+    }
 })
